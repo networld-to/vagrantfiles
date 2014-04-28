6a770e531f00552a993f65698b3e8da2f87b98b2
Alex Oberhauser <alex.oberhauser@networld.to>
2014-04-28 13:57:19 +0200

    Fixing apt interactive problem during rvm installation.

--- debian7.4/install-ruby.sh
+++ debian7.4/install-ruby.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source /usr/local/rvm/scripts/rvm
+export DEBIAN_FRONTEND=noninteractive
 
 rvm use --install $1
 
--- debian7.4/install-rvm.sh
+++ debian7.4/install-rvm.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+export DEBIAN_FRONTEND=noninteractive
 curl -sSL https://get.rvm.io | bash -s $1
