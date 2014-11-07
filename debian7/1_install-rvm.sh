3ff2f1ead012edc0775e0d2fd35efd186e2d19f4
Alex Oberhauser <alex.oberhauser@networld.to>
2014-11-07 17:00:23 +0100

    Fixing missing gpg key for rvm and upgrading debian to 7.6

--- debian7/1_install-rvm.sh
+++ debian7/1_install-rvm.sh
@@ -3,6 +3,7 @@
 rvm_installed=$(rvm --version &> /dev/null; echo $?)
 
 if ! [ ${rvm_installed} == 0 ]; then
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
   export DEBIAN_FRONTEND=noninteractive
   curl -sSL https://get.rvm.io | bash -s $1
 else
